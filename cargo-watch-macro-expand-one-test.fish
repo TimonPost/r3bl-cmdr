64c781be0913e661f90b8ee834509c9c2ef8cd75
Nazmul Idris <nazmul@fasterlap.com>
2022-07-06 09:46:22 -0700

    Update fish scripts

--- cargo-watch-macro-expand-one-test.fish
+++ cargo-watch-macro-expand-one-test.fish
@@ -20,4 +20,7 @@ end
 # 2. cargo test -- --help   => these go to the binary that is being tested
 
 # cargo watch -x check -x "ltest $argv --color always -q --message-format short  -- --nocapture" -c -q
-RUST_BACKTRACE=1 RUSTFLAGS=-Awarnings cargo watch -x "expand --test $argv"
+RUST_BACKTRACE=1 cargo watch -x "expand --test $argv"
+
+# The following command will rebuild every time (no caching) but will not show warnings.
+# RUST_BACKTRACE=1 RUSTFLAGS=-Awarnings cargo watch -x "expand --test $argv"
--- run.fish
+++ run.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 # cargo update
 # cargo build --release
-RUSTFLAGS=-Awarnings RUST_BACKTRACE=1 cargo run
+RUST_BACKTRACE=1 cargo run
